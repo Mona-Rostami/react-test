cc3bd3fddc9e776e0fd7eee58f9509ddd5d68f24
mona <rostami.m4813@gmail.com>
2025-04-10 10:38:06 -0700

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Aboutuse from "./pages/about/aboutUs";
 import Home from "./pages/home/home";
-// import { Route,Routes } from "react-router-dom";
 import Nextpage from "./pages/pagebox/linkbox";
-import { HashRouter as Router,Route,Routes } from "react-router-dom";
+import { HashRouter as Route,Routes } from "react-router-dom";
   function App() {
   return (
-     <Router>
+     
       <div>
         <Routes>
         <Route path="/" element={<Home />}/>
@@ -13,7 +12,7 @@ import { HashRouter as Router,Route,Routes } from "react-router-dom";
       <Route path="/newp/:id" element={<Nextpage/>}></Route>
         </Routes>
       </div>
-     </Router>
+    
    
   );
 }
