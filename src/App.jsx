3c2adc51e056eed1d0329dc35102d31bd64dd14f
mona <rostami.m4813@gmail.com>
2025-04-10 07:54:07 -0700

    HashRouter and base config for GitHub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import Aboutuse from "./pages/about/aboutUs";
 import Home from "./pages/home/home";
-import { Route,Routes } from "react-router-dom";
+// import { Route,Routes } from "react-router-dom";
 import Nextpage from "./pages/pagebox/linkbox";
+import { HashRouter as Router,Route,Routes } from "react-router-dom";
   function App() {
   return (
-    <div>
-     <Routes>
-      <Route path="/" element={<Home />}/>
+     <Router>
+      <div>
+        <Routes>
+        <Route path="/" element={<Home />}/>
       <Route path="/about"element={<Aboutuse/>}/>
       <Route path="/newp/:id" element={<Nextpage/>}></Route>
-     </Routes>
-
-    </div>
+        </Routes>
+      </div>
+     </Router>
+   
   );
 }
 
